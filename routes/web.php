09d802163f3153a21c0422868204411d40b8da4c
leonardo casales! <leonardo@smartnet.com.ar>
2018-06-17 22:25:24 -0300

    model, controller, migration added

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('movies', 'MoviesController');
+
+Route::resource('movie', 'MoviesController');
 
 Route::get('/', function () {
     return view('welcome');
