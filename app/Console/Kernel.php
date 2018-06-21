2f98303d0dc7329f699a3f422d107d3d9828710b
Leonardo Casales <leonardo@smartnet.com.ar>
2018-06-21 13:14:26 -0300

    command created

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'App\Console\Commands\SaveApiMovieToDB.php',
     ];
 
     /**
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('SaveApiMovieToDB:saveMovie')
+                    ->everyMinute();
     }
 
     /**
